2959be3edc08936b96016f8bc92b11898428bc38
Your Name <trandina.e@gmail.com>
2022-12-06 15:22:42 +0300

    add Lesson 2 Example 013

---
+++ Lesson2_013/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+что третьей цифры нет*/
+
+Console.WriteLine("Введите число : ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите индекс числа : ");
+int index = Convert.ToInt32(Console.ReadLine());
+
+double pow = Math.Pow(10, index);
+Console.Clear();
+
+if (num < pow)
+{
+    Console.WriteLine($"Третьей цифры нет");
+    return;
+}
+
+int tmp = num;
+
+while (tmp > pow)
+{
+    tmp /= 10;
+}
+int result = tmp % 10;
+Console.WriteLine($"Цифра под индексом {index} числа {num}: {result}");
+
